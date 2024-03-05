eff22c6c187c47df09417b13dd58d5b74a9eafb3
Brandon <Bjennin4@kent.edu>
2024-03-04 22:11:03 -0500

    Created method to save levels

--- levelMaker.js
+++ levelMaker.js
@@ -30,11 +30,9 @@ function setup() {
     floor7.rotationLock = true;
 }
 
-// let mapObject0 = {}; // Array of tiles on the map
+// Array of tiles on the map
 let mapObject0 = [];
-// mapObject0.coords = [];
 tileSize = 16;
-// mapObject0.floor7 = new Sprite();
 
 
 function draw() {
@@ -49,9 +47,36 @@ function draw() {
     if (keyIsDown(RIGHT_ARROW)) {} // Move canvas right
     else if(keyIsDown(LEFT_ARROW)) {} // Move canvas left
 
-    // if(keyPressed(ENTER)){
-    //     // Save Canvas
-    // }
+    // Used to detect Enter key press
+    if(keyIsDown(ENTER)){
+        // Prevents continuous key reads
+        if(keyCode === ENTER){
+            // Save Canvas
+            let mapObject0JSON = [];
+            let tmpStr = {};
+
+            // Converts the Sprite into a non sprite object, because sprites cannot be saved to JSON files
+            mapObject0.forEach(element => {
+                tmpObj = {
+                    "img" : "",
+                    "collider" : "",
+                    "x" : 0,
+                    "y" : 0
+                };
+                tmpObj.img = element.Sprite.img.url;
+                tmpObj.collider = element.Sprite.collider;
+                tmpObj.x = element.Sprite.x;
+                tmpObj.y = element.Sprite.y;
+                tmpStr = JSON.stringify(tmpObj);
+                mapObject0JSON.push(tmpStr);
+            });
+
+            saveJSON(mapObject0JSON, 'map0.json');
+            print(mapObject0JSON);
+            print("Canvas Saved");
+            keyCode = null;
+        }
+    }
 }
 
 function setTile(){
@@ -64,11 +89,14 @@ function setTile(){
 
     // Setting tile attributes
     tmpTile.name = "Tile " + X + " " + Y;
-    tmpTile.sprite = new Sprite();
-    tmpTile.sprite.img = 'assets/levelTiles/Dungeon tileset/tile001.png';
-    tmpTile.sprite.collider = 'static';
-    tmpTile.sprite.x = X;
-    tmpTile.sprite.y = Y;
+
+    tmpSprite = new Sprite();
+    tmpSprite.img = 'assets/levelTiles/Dungeon tileset/tile007.png';
+    tmpSprite.collider = 'static';
+    tmpSprite.x = X;
+    tmpSprite.y = Y;
+
+    tmpTile.Sprite = tmpSprite;
 
     // Adding the tile into the level map
     mapObject0.push(tmpTile);
