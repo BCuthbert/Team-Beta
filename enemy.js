208ae5c312950086ad7f64869bcc05fedfc63537
2Beast101 <bradendavidc@yahoo.com>
2024-04-14 17:47:25 -0400

    Enemies spawn in respective positions

--- enemy.js
+++ enemy.js
@@ -1,14 +1,25 @@
 
 
+
+
 export function enemy(p) {
     return {
         sprite: null,
+        enemies: null,
         animations: {},
         health: 1,
         speed: 3,
         isDead: false,
 
+
         setup() {
+            this.enemies = new p.Group();
+            this.enemies.collider = 'none';
+            this.enemies.scale = 2;
+            console.log(p.minute());
+        },
+
+        preload() {
             this.loadAnimations();
         },
 
@@ -20,6 +31,14 @@ export function enemy(p) {
             this.animations.run = p.loadAnimation('assets/golemIdle.png',
                 { frameSize: [16, 16], frames: 4 });
             this.animations.run.frameDelay = 5;
+        },
+
+        spawn(x, y) {
+
+            this.sprite = new this.enemies.Sprite(x, y);
+            this.sprite.mode = 'UNAWARE';
+            // console.log(this.sprite.mode);
+            this.sprite.addAni(this.animations.idle);
         },
 
     };
