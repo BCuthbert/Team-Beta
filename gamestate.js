faced3506139f9e8bfd0add50398980ee56b0d21
snowman1701 <101939615+snowman1701@users.noreply.github.com>
2024-05-01 15:53:45 -0400

    Update gamestate.js
    reloads the game

--- gamestate.js
+++ gamestate.js
@@ -2,7 +2,7 @@
 
 export function GameState(p) {
     return {
-        states: { 0: 'menu', 1: 'level1' },
+        states: { 0: 'menu', 1: 'level1', 2: 'tryagain' },
         currentState: 0, // currentState- 0 is menu, 1 is level1, etc.
 
 
@@ -11,7 +11,14 @@ export function GameState(p) {
         },
 
         changeState(state) {
+            if (state === 'tryagain'){
+                console.log("Reloading Game");
+                setTimeout(() => {
+                    window.location.reload();
+                    }, 2000);
+            } else {
             this.currentState = this.states[this.currentState];
+            }
         },
 
     };
